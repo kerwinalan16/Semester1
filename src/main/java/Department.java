8fb42766d73ef1ffad3b8829bae1ffc53fa36f87
alan <kerwinalan16@gmail.com>
2025-12-05 10:47:03 -0500

    I made departmentId final, finished the method isDepartmentNameValid by making it so that only letters and spaces are allowed. Addded the constructor as well.

--- src/main/java/Department.java
+++ src/main/java/Department.java
@@ -7,12 +7,32 @@ import lombok.ToString;
 @EqualsAndHashCode
 @Getter
 public class Department {
-    private String departmentId;
-   @Setter private String departmentName;
+    private final String departmentId;
+    @Setter
+    private String departmentName;
     private static int nextId = 1;
 
     public static boolean isDepartmentNameValid(String departmentName) {
         if (departmentName == null || departmentName.isEmpty()) {
             return false;
         }
+        for (char character : departmentName.toCharArray()) {
+            if (!Character.isLetter(character) && character != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("D%02d", nextId++);
+            this.departmentName = departmentName;
+        } else {
+
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
 }
