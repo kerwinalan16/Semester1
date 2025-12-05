f2be36c41db273bb8457fca3b189fd46f8747158
alan <kerwinalan16@gmail.com>
2025-12-05 10:34:28 -0500

    Added the fields and made the method for the department class

--- src/main/java/Department.java
+++ src/main/java/Department.java
@@ -1,2 +1,18 @@
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Department {
+    private String departmentId;
+   @Setter private String departmentName;
+    private static int nextId = 1;
+
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
 }
