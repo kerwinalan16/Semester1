cf1c98f5927a69ca19d67ea8c3fb70f551f7ad32
alan <kerwinalan16@gmail.com>
2025-12-07 20:49:26 -0500

    Equals, getter, setters. Added the fields and constructor for the student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,30 @@
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+@Getter
+@EqualsAndHashCode
+@Setter
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private List<Course> registeredCourses;
+
+    private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("%06d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new List<Course>();
+    }
+    public enum Gender{
+        Male, Female
+    }
 }
