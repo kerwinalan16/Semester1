387b6fb925e2dd0c6cca7963d3647e88d05aca25
alan <kerwinalan16@gmail.com>
2025-12-14 16:21:43 -0500

    tested isPostalCodeValid method of the address class

--- src/test/java/ClassTests.java
+++ src/test/java/ClassTests.java
@@ -1,2 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+//Address class
 public class ClassTests {
+    @Test
+    @DisplayName("isPostalCodeValid: A1B2C3 -> true")
+    void isPostalCodeValidTest1() {
+        String postalCode = "A1B2C3";
+        boolean expected = true;
+        boolean actual = Address.isPostalCodeValid(postalCode);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: A1B2C -> false")
+    void isPostalCodeValidTest2() {
+        String postalCode = "A1B2C";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(postalCode);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: 123ABC -> false")
+    void isPostalCodeValidTest3() {
+        String postalCode = "123ABC";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(postalCode);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+
 }
