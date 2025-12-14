0a040fae0457da369396e4a6bb5a43402acd2f0a
alan <kerwinalan16@gmail.com>
2025-12-14 16:29:10 -0500

    tested isDepartmentNameValid method of the department class

--- src/test/java/ClassTests.java
+++ src/test/java/ClassTests.java
@@ -31,7 +31,35 @@ public class ClassTests {
         Assertions.assertEquals(expected, actual);
     }
 
+        @Test
+        @DisplayName("isDepartmentNameValid: Computer Science -> true")
+        void isDepartmentNameValidTest1() {
+            String departmentName = "Computer Science";
+            boolean expected = true;
+            boolean actual = Department.isDepartmentNameValid(departmentName);
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        @DisplayName("isDepartmentNameValid: Math123 -> false")
+        void isDepartmentNameValidTest2() {
+            String departmentName = "Math123";
+            boolean expected = false;
+            boolean actual = Department.isDepartmentNameValid(departmentName);
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        @DisplayName("isDepartmentNameValid: Physics! -> false")
+        void isDepartmentNameValidTest3() {
+            String departmentName = "Physics!";
+            boolean expected = false;
+            boolean actual = Department.isDepartmentNameValid(departmentName);
+            Assertions.assertEquals(expected, actual);
+        }
+    }
 
 
 
-}
+
+
