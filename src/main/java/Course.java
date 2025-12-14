8bec39940ec4d7db87c241478e5d56f68748347d
alan <kerwinalan16@gmail.com>
2025-12-14 14:00:53 -0500

    added fields for the course class

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private List<Assignment> assignments;
+    private List<Student> registeredStudents;
+
+    private static int nextId = 1;
+
 }
+
+
+
