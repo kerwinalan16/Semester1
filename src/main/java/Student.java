1f33006dba64b61e69884b62adad6486bfc232e4
alan <kerwinalan16@gmail.com>
2025-12-14 15:50:29 -0500

    wrote registerCourse(Course course) method and java doc

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,6 +3,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 @EqualsAndHashCode
 @Setter
@@ -22,9 +25,48 @@ public class Student {
         this.gender = gender;
         this.address = address;
         this.department = department;
-        this.registeredCourses = new List<Course>();
+        this.registeredCourses = new ArrayList<>();
+
     }
     public enum Gender{
         Male, Female
     }
-}
+
+    /**
+     * Registers a course for this student.
+     * 1) Adds the course to the student's registeredCourses list.
+     * 2) Adds this student to the course's registeredStudents list.
+     * 3) Appends a null for the scores of each assignment of the course.
+     * @param course the course to register
+     * @return true if registration succeeded, false if already registeredad
+     */
+    public boolean registerCourse(Course course) {
+        if (registeredCourses.contains(course)) {
+            return false;
+        }
+
+        registeredCourses.add(course);
+        course.getRegisteredStudents().add(this);
+
+
+        for (Assignment assignment : course.getAssignments()) {
+            assignment.getScores().add(null);
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
